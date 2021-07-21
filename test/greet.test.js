792c38bd3c82571d0ddd5f44263eb490e14d4eec
xolile2000 <xolilentanzi43@gmail.com>
2021-07-21 11:51:21 +0200

    first commit

---
+++ test/greet.test.js
@@ -0,0 +1,14 @@
+let assert = require("assert");
+var greet = require('../greet');
+describe('The greet function', function(){
+
+    it('should be able to greet bob', function(){
+        assert.equal('Hello ,bob', greet('bob'));
+    });
+    describe('The greet function', function(){
+
+        it('should be able to greet Xolile', function(){
+            assert.equal('Hello ,Xolile', greet('Xolile'));
+        });
+});
+});
