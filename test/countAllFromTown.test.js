b760b16611f5bd517836e7df55b5fc968d25bbc3
xolile2000 <xolilentanzi43@gmail.com>
2021-07-22 13:15:51 +0200

    update

--- test/countAllFromTown.test.js
+++ test/countAllFromTown.test.js
@@ -5,7 +5,7 @@ describe('countAllFromTown', function(){
     it('should be able to return the number of registration numbers from stallies town', function(){
         assert.equal(3, countAllFromTown('CL 124,CY 567,CL 345, CJ 456,CL 341','CL'));
     });
-    it('should be able to return the number of registration numbers from a kuilsriver town', function(){
+    it('should be able to return the number of registration numbers from a stallies town', function(){
         assert.equal(1, countAllFromTown('CJ 124,CY 567,CL 345, CF 456, CL 341','CF'));
     });
 
